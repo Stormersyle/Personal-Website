dee86680b80ed1a61b6e08d621af40b8feb3863f
Andrew <andrewyuan112@gmail.com>
2024-03-28 17:51:22 -0400

    setup + skeleton

---
+++ client/src/components/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../stylesheets/home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Andrew Yuan</h1>
+      <p>
+        Hi! I'm Andrew, a freshman at MIT studying math and computer science.
+        Welcome to my personal website!
+      </p>
+    </div>
+  );
+};
+
+return Home;
---
+++ client/src/index.js
@@ -0,0 +1,34 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./stylesheets/index.css";
+
+import Home from "./components/home.js";
+import Academics from "./components/academics.js";
+import ProjectsPage from "./components/projects_page.js";
+import Blog from "./components/blog.js";
+import Contact from "./components/contact.js";
+import { NewBlog, NewProject } from "./components/new_post.js";
+import Delete from "./components/delete.js";
+import NavBar from "./components/navbar.js";
+import Footer from "./components/footer.js";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/academics", element: <Academics /> },
+  { path: "/projects", element: <ProjectsPage /> },
+  { path: "/blog", element: <Blog /> },
+  { path: "/contact", element: <Contact /> }, //next three are private pages
+  { path: "/newblog", element: <NewBlog /> },
+  { path: "/newproject", element: <NewProject /> },
+  { path: "/delete", element: <Delete /> },
+]);
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <NavBar />
+    <RouterProvider router={router} />
+    <Footer />
+  </StrictMode>
+);
