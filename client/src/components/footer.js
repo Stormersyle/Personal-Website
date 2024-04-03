44ff81bad1325f53a468fa49d995793a06571b12
Andrew <andrewyuan112@gmail.com>
2024-04-03 16:08:20 -0400

    more formatting changes

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -4,17 +4,19 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="footer">
-      <p className="u-inline">© 2024. Designed & developed by Andrew Yuan. </p>
+      <p className="u-inline u-m">© 2024. Designed & developed by Andrew Yuan. </p>
       <div className="u-flex u-justify-center u-align-center">
         <a
           href="https://github.com/Stormersyle/Personal-Website"
           target="_blank"
-          className="source-code"
+          className="source-code u-m"
         >
           Source Code
         </a>
         &nbsp;|&nbsp;
-        <Link to="/admin">Admin Panel</Link>
+        <Link to="/admin" className="u-m">
+          Admin Panel
+        </Link>
       </div>
     </div>
   );
