7c87a7d6803f20fe91c9ef736d586da029cf0891
Andrew <andrewyuan112@gmail.com>
2024-06-03 08:34:03 -0400

    added links to blog editor

--- client/src/components/tiptap.js
+++ client/src/components/tiptap.js
@@ -4,12 +4,30 @@ import { Color } from "@tiptap/extension-color";
 import ListItem from "@tiptap/extension-list-item";
 import TextStyle from "@tiptap/extension-text-style";
 import Heading from "@tiptap/extension-heading";
+import Link from "@tiptap/extension-link";
 import { useEditor, EditorContent } from "@tiptap/react";
 import { generateHTML } from "@tiptap/html";
 import StarterKit from "@tiptap/starter-kit";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 const MenuBar = ({ editor }) => {
+  const setLink = useCallback(() => {
+    const previousUrl = editor.getAttributes("link").href;
+    const url = window.prompt("URL", previousUrl);
+    // cancelled
+    if (url === null) {
+      return;
+    }
+    // empty
+    if (url === "") {
+      editor.chain().focus().extendMarkRange("link").unsetLink().run();
+
+      return;
+    }
+    // update link
+    editor.chain().focus().extendMarkRange("link").setLink({ href: url }).run();
+  }, [editor]);
+
   if (!editor) {
     return null;
   }
@@ -37,6 +55,17 @@ const MenuBar = ({ editor }) => {
       >
         strike
       </button>
+
+      <button onClick={setLink} className={editor.isActive("link") ? "is-active" : ""}>
+        setLink
+      </button>
+      <button
+        onClick={() => editor.chain().focus().unsetLink().run()}
+        disabled={!editor.isActive("link")}
+      >
+        unsetLink
+      </button>
+
       <button
         onClick={() => editor.chain().focus().toggleCode().run()}
         disabled={!editor.can().chain().focus().toggleCode().run()}
@@ -113,6 +142,7 @@ const extensions = [
   Color.configure({ types: [TextStyle.name, ListItem.name] }),
   TextStyle.configure({ types: [ListItem.name] }),
   Heading,
+  Link.configure({ autolink: false, openOnClick: false }),
   StarterKit.configure({
     bulletList: {
       keepMarks: true,
